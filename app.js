d4a55bff70b74685a314ee96683c232862339de4
Patrice Sorrant <patrice.sorrant@gmail.com>
2023-04-14 16:58:56 -0400

    Attack, heal, special attack functionalities up, Win/Loss messages up, Game over / Play again up,

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             playerPower: 15,
-            healingPower: 15,
+            healingPower: 12,
             monsterHealth: 100,
             monsterPower: 20,
             currentRound: 0,
             isGameOver: false,
+            isPlayerWinner: null,
         };
     },
     methods: {
@@ -33,18 +34,19 @@ const app = Vue.createApp({
             this.attackPlayer();
         },
         specialAttack() {
-            if (this.currentRound % 3 === 0) {
-                this.currentRound += 1;
-                const damageDealt = this.strikeCalc(this.playerPower * 2);
-                this.monsterHealth = Math.max(
-                    this.monsterHealth - damageDealt,
-                    0
-                );
-                this.attackPlayer();
-            }
+            this.currentRound += 1;
+            const damageDealt = this.strikeCalc(this.playerPower * 2);
+            this.monsterHealth = Math.max(this.monsterHealth - damageDealt, 0);
+            this.attackPlayer();
         },
         surrender() {
             this.isGameOver = true;
+        },
+        startNewGame() {
+            this.isGameOver = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
         },
     },
     computed: {
@@ -54,11 +56,25 @@ const app = Vue.createApp({
         playerBarStyle() {
             return { width: this.playerHealth + "%" };
         },
+        isSpecialAttack() {
+            return this.currentRound % 3 === 0;
+        },
     },
     watch: {
         playerHealth(newHealth) {
-            if (newHealth <= 0) {
-                window.alert("HAAAAA");
+            if (newHealth === 0) {
+                setTimeout(() => {
+                    this.isGameOver = true;
+                    this.isPlayerWinner = false;
+                }, 10);
+            }
+        },
+        monsterHealth(newHealth) {
+            if (newHealth === 0) {
+                setTimeout(() => {
+                    this.isGameOver = true;
+                    this.isPlayerWinner = true;
+                }, 10);
             }
         },
     },
